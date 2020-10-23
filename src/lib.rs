85121942d6216c0bbeee5ee2d67cb142c40b929d
Muhammad Mominul Huque <mominul2082@gmail.com>
2020-10-24 01:55:22 +0600

    More rules

--- src/lib.rs
+++ src/lib.rs
@@ -4,24 +4,48 @@ pub fn generate_regex(word: &str) -> String {
     output.push('^');
 
     let mut chars = word.chars().peekable();
+    let mut index = 0;
     while let Some(character) = chars.next() {
         match character.to_ascii_lowercase() {
-            'a' => output.push_str("(অ্যা|্যা|া)"),
-            'c' => output.push_str("(ক)"),
+            'a' => output.push_str("(অ্যা|্যা|া|ে)"),
+            'b' => output.push_str("ব?"),
+            'c' => {
+                if let Some(next) = chars.peek() {
+                    // ck -> ক
+                    if next.to_ascii_lowercase() == 'k' {
+                        output.push_str("ক");
+                        // Eat the 'k' character.
+                        chars.next();
+                        index += 2;
+                        continue;
+                    } else if next.to_ascii_lowercase() == 'h' {
+                        // ch -> চ
+                        output.push_str("চ");
+                        // Eat the 'h' character.
+                        chars.next();
+                        index += 2;
+                        continue;
+                    }
+                }
+                output.push_str("(ক)");
+            }
             'd' => output.push_str("(ড)"),
-            'e' => output.push_str("(ে|ই)"),
-            'g' => output.push_str("(গ|জ)"),
+            'e' => output.push_str("(ে|ি|ই)?"),
+            'f' => output.push_str("ফ"),
+            'g' => output.push_str("(গ|জ)?"),
             'h' => output.push_str("(হ)?"),
             'i' => output.push_str("(ই|ি)"),
+            'k' => output.push_str("(ক)"),
             'l' => output.push_str("(ল)"),
             'm' => output.push_str("(ম)?"),
             'n' => {
-                // ng => ং
+                // ng -> ং
                 if let Some(next) = chars.peek() {
                     if next.to_ascii_lowercase() == 'g' {
                         output.push_str("ং");
                         // Eat the 'g' character.
                         chars.next();
+                        index += 2;
                         continue;
                     }
                 }
@@ -29,13 +53,31 @@ pub fn generate_regex(word: &str) -> String {
             }
             'o' => output.push_str("(ও|ো)?"),
             'p' => output.push_str("প"),
-            'r' => output.push_str("(র|্র)"),
-            's' => output.push_str("(শ|স)"),
+            'q' => output.push_str("ক"),
+            'r' => output.push_str("(র|্র|র্)"),
+            's' => output.push_str("(শ|স)?"),
+            't' => {
+                // tion -> শন
+                if let Some(next) = word.get(index..=index+3) {
+                    if next.to_ascii_lowercase() == "tion" {
+                        output.push_str("শন");
+                        // Eat the 'ion' characters.
+                        chars.next();
+                        chars.next();
+                        chars.next();
+                        index += 4;
+                        continue;
+                    }
+                }
+                output.push_str("(ট)?");
+            }
+            'u' => output.push_str("(ু|িউ|া)"),
             _ => ()
         }
 
         // Add optional Hashanta
         output.push_str("্?");
+        index += 1;
     }
     output.push('$');
 
@@ -61,5 +103,32 @@ mod tests {
 
         regex = Regex::new(&generate_regex("programming")).unwrap();
         assert!(regex.is_match("প্রোগ্রামিং"));
+
+        regex = Regex::new(&generate_regex("suggestion")).unwrap();
+        assert!(regex.is_match("সাজেশন"));
+
+        regex = Regex::new(&generate_regex("start")).unwrap();
+        assert!(regex.is_match("স্টার্ট"));
+
+        regex = Regex::new(&generate_regex("chicken")).unwrap();
+        assert!(regex.is_match("চিকেন"));
+
+        regex = Regex::new(&generate_regex("attribute")).unwrap();
+        assert!(regex.is_match("অ্যাট্রিবিউট"));
+
+        regex = Regex::new(&generate_regex("package")).unwrap();
+        assert!(regex.is_match("প্যাকেজ"));
+
+        regex = Regex::new(&generate_regex("quick")).unwrap();
+        assert!(regex.is_match("কুইক"));
+
+        regex = Regex::new(&generate_regex("computation")).unwrap();
+        assert!(regex.is_match("কম্পিউটেশন"));
+
+        regex = Regex::new(&generate_regex("feedback")).unwrap();
+        assert!(regex.is_match("ফিডব্যাক"));
+
+        regex = Regex::new(&generate_regex("clock")).unwrap();
+        assert!(regex.is_match("ক্লক"));
     }
 }
